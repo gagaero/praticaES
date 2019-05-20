46d72477d81b9dee495d9c0d743ea3477e3959e3
gagaero <49010114+gagaero@users.noreply.github.com>
2019-05-20 13:45:30 -0300

    A

--- class.java
+++ class.java
@@ -1,24 +1,36 @@
-class Money {
-   ...
+abstract class Money {
+   private String currency; 
+
    static Money dollar(int amount)  {
-      return new Dollar(amount);
+      return new Dollar(amount, "USD");
    }
-   
+
    static Money franc(int amount) {
-      return new Franc(amount);
-    }
-} 
+      return new Franc(amount, "CHF");
+   }
 
-class Dollar {
-   ...
-   Money times(int multiplier)  {
-      return new Dollar(amount * multiplier);
-   }								
-}    
+   Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
+   }
+}
 
-class Franc {
-   ...
+class Franc extends Money {	
+   Franc(int amount, String currency) {
+      super(amount, currency);
+   }
+     
    Money times(int multiplier)  {
-      return new Franc(amount * multiplier);
-   }								
-}    
+      return Money.franc(amount * multiplier);
+   }
+}
+
+class Dollar extends Money {	
+   Dollar(int amount, String currency)  {
+      super(amount, currency);
+   }
+	
+   Money times(int multiplier)  {
+      return Money.dollar(amount * multiplier);
+   }
+}
